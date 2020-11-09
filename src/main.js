9a390db020e0ddfd75b647da92978c486c98fc67
TBchip <bischoffthijs@gmail.com>
2020-11-09 20:38:06 +0100

    added registerattime function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 const { registerLPUs } = require("./register")
 
-// LPUYPositions = [264, 776, 968, 1224, 1288, 1320];
-LPUYPositions = [680];
+async function registerLPUsAtTime(targetDay, targetHour, targetMinute){
+    while(true){
+        let date = new Date();
 
-registerLPUs(LPUYPositions);
+        let day = date.getDay();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        
+        if(day === targetDay && hour === targetHour && minute === targetMinute){
+            registerLPUs(LPUYPositions);
+        }
+        console.log(minute);
+
+        await sleep(60000);
+    }
+}
+
+
+LPUYPositions = [264, 776, 968, 1224, 1288, 1320];
+// LPUYPositions = [680]; 
+
+registerLPUsAtTime(1, 9, 5);
+
