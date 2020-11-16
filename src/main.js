4de15feae1d138b0442ffb06993eb1d7ace15e97
TBchip <bischoffthijs@gmail.com>
2020-11-16 17:29:44 +0100

    changed some shit

--- src/main.js
+++ src/main.js
@@ -1,25 +1,45 @@
 const { registerLPUs } = require("./register")
 
-async function registerLPUsAtTime(targetDay, targetHour, targetMinute){
+async function registerLPUsAtTime(targetDay, targetHour, targetMinute, tries){
+    let triesLeft = tries;
+    let shouldTry = false;
     while(true){
         let date = new Date();
-
         let day = date.getDay();
         let hour = date.getHours();
         let minute = date.getMinutes();
         
-        console.log(`Checking time: ${day}, ${hour}, ${minute}`);
-        if(day === targetDay && hour === targetHour && minute === targetMinute){
+        console.log(`Current time: ${day}, ${hour}, ${minute}`);
+        if((day === targetDay && hour === targetHour && minute === targetMinute) && !shouldTry){
             console.log("Correct time, registering for LPUs");
             console.log();
             console.log();
-            registerLPUs(LPUYPositions);
+            shouldTry = true;
         }
-        else
+        else if(!shouldTry){
             console.log("Wrong time, sleeping for 1 minute");
+        }
+
+
+        if(shouldTry){
+            console.log("----------");
+            let res = await registerLPUs(LPUYPositions);
+            console.log("----------");
+
+            triesLeft--;
+            if(res === true || triesLeft === 0){
+                shouldTry = false;
+                triesLeft = tries;
+            }else{
+                console.log("Error while registering, trying again in 1 minute")
+                console.log();
+                console.log();
+            }
+        }
+        
         console.log();
 
-        await sleep(60000);
+        await sleep(60000); 
     }
 }
 function sleep(ms) {
@@ -33,5 +53,5 @@ LPUYPositions = [264, 776, 968, 1224, 1288, 1320];
 // LPUYPositions = [680]; 
 // registerLPUs(LPUYPositions);
 
-registerLPUsAtTime(1, 9, 5);
+registerLPUsAtTime(1, 17, 26, 1);
 
